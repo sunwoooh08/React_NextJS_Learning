0d03c60c7c15148edbead1ab9467b2c1d91a33d2
chkbx82 <contact@previousw.dev>
2026-09-13 19:21:43 +0900

    feat: implement counter to learn component, props, and useState structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
-const NameDisplayer = (name: String) => {
-  return (
-    <main>
-      <h1>Hello, {name}!</h1>
-    </main>
-  );
-}
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  const name = "Sunwoo";
+
+  const [count, setCount] = useState(0);
+
+  function onClickHandler(incDec: "inc" | "dec") {
+    if (incDec == "inc") {
+      setCount(count + 1);
+    } else {
+      setCount(count - 1);
+    }
+  }
+
+  type ClickButtonProps = {
+    incDec: "inc" | "dec";
+  }
   
-  return NameDisplayer(name);
+  function ClickButton({incDec}: ClickButtonProps) {
+    return (
+      <div>
+        <button onClick={() => { onClickHandler(incDec) }}>1 만큼을 {incDec == "inc" ? "추가하려면" : "감소시키려면"} 클릭하세요.</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <ClickButton incDec="inc" />
+      <ClickButton incDec="dec" />
+      <p>Count: {count}</p>
+    </div>
+  );
 }
